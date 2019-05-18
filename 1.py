d21029ef25d1c0848652afd8247acdc8bd404bbb
Raj Madheshia <32214632+Raj-Madheshia@users.noreply.github.com>
2019-05-18 12:01:16 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,7 +2,6 @@ import speech_recognition as sr
 from time import ctime
 import time
 import os
-from gtts import gTTS
 import pyttsx3
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
